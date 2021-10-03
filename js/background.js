7097c24d11ee75e2729b16a752025f6d006325ed
unbroken2650 <unbroken2650@gmail.com>
2021-10-03 04:47:11 +0000

    background change

--- js/background.js
+++ js/background.js
@@ -11,12 +11,7 @@ const backgroundImage = document.createElement("style");
 backgroundImage.type = "text/css";
 backgroundImage.innerText = `body{
     background-image: url("img/${chosenImage}");
-    height: 100%;
-    width: 100%;
-    background-position: center;
-    background-attachment: scroll;
-    background-repeat: no-repeat;
-    background-size: cover;
+    
         }
 `;
 
