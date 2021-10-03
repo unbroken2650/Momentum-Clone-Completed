d2402ba844ff003035719e8d47daa410550a7247
unbroken2650 <unbroken2650@gmail.com>
2021-10-03 04:14:12 +0000

    added backgroundimage

--- js/background.js
+++ js/background.js
@@ -1,12 +1,23 @@
 const images =[
-  "0.jpg",
-  "1.jpg",
-  "2.jpg",
+    "background (1).jpg",
+    "background (2).jpg",
+    "background (3).jpg",
+    "background (4).jpg",
 ];
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
-const backgroundImage = document.createElement("img");
-backgroundImage.src = `img/${chosenImage}`;
+const backgroundImage = document.createElement("style");
+backgroundImage.type = "text/css";
+backgroundImage.innerText = `body{
+    background-image: url("img/${chosenImage}");
+    height: 100%;
+    width: 100%;
+    background-position: center;
+    background-attachment: scroll;
+    background-repeat: no-repeat;
+    background-size: cover;
+        }
+`;
 
-document.body.appendChild(backgroundImage);
+document.head.appendChild(backgroundImage);
